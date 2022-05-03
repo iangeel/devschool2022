a3a71a02058e04cfed7e392ba1df67f0d80ec693
Angel Ivana <angel.ivana@ing.com>
2022-05-03 07:53:25 +0300

    - hash password using bcrypt at compile

--- ss-simple-api/src/main/java/com/ing/devschool/security/SecurityConfiguration.java
+++ ss-simple-api/src/main/java/com/ing/devschool/security/SecurityConfiguration.java
@@ -9,15 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -31,16 +28,14 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .csrf().disable();
 
     }
+
     @Bean
     public UserDetailsService users() {
         UserDetails user = User.builder()
-                .username("user1")
-                .password(passwordEncoder.encode("parola"))
+                .username("user")
+                .password("{bcrypt}$2a$10$ZiZQgMttbZKGgW2LxDyZwe90cyBNiWXVfgUrKm6ARtJUpjAm5BcCi")
                 .roles("CLIENT")
                 .build();
         return new InMemoryUserDetailsManager(user);
     }
 }
-
-
-//{bcrypt}$2a$10$ZiZQgMttbZKGgW2LxDyZwe90cyBNiWXVfgUrKm6ARtJUpjAm5BcCi
